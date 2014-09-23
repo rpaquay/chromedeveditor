b0ed0632e3e8eefbe6612d87e33263d9ea92c425
Renaud Paquay <rpaquay@chromium.org>
2014-09-23 12:32:58 -0700

    Removed unused "buildFiles" method.

--- ide/web/lib/services/analyzer.dart
+++ ide/web/lib/services/analyzer.dart
@@ -87,46 +87,6 @@ class AnalyzerDartServices implements DartServices {
     return new Future.value(declaration);
   }
 
-  Future<Map<String, List<Map>>> buildFiles(List<Map> fileUuids) {
-    Map<String, List<Map>> errorsPerFile = {};
-
-    return dartSdkFuture.then((ChromeDartSdk sdk) {
-      return Future.forEach(fileUuids, (String fileUuid) {
-        return _processFile(sdk, fileUuid).then((AnalyzerResult result) {
-          List<AnalysisError> errors = result.errors;
-          List<Map> responseErrors = [];
-
-          if (errors != null) {
-            for (AnalysisError error in errors) {
-              common.AnalysisError responseError = new common.AnalysisError();
-              responseError.message = error.message;
-              responseError.offset = error.offset;
-              LineInfo_Location location = result.getLineInfo(error);
-              responseError.lineNumber = location.lineNumber;
-              responseError.errorSeverity =
-                  _errorSeverityToInt(error.errorCode.errorSeverity);
-              responseError.length = error.length;
-              responseErrors.add(responseError.toMap());
-            }
-          }
-
-          return responseErrors;
-        }).then((List<Map> errors) {
-          errorsPerFile[fileUuid] = errors;
-        });
-      });
-    }).then((_) => errorsPerFile);
-  }
-
-  /**
-   * Analyzes file and returns a Future with the [AnalyzerResult].
-   */
-  Future<AnalyzerResult> _processFile(ChromeDartSdk sdk, String fileUuid) {
-    return _contentsProvider
-        .getFileContents(fileUuid)
-        .then((String contents) => analyzeString(sdk, contents));
-  }
-
   common.Declaration _getDeclarationFor(
       ProjectContext context, String fileUuid, int offset) {
     WorkspaceSource source = context.getSource(fileUuid);
