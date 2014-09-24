80e78633cfee5b945bf4928185fdffb941571245
Renaud Paquay <rpaquay@chromium.org>
2014-09-24 13:01:57 -0700

    Update batch file to checkout [analyzer] package too.

--- svn_analyis_server.cmd
+++ svn_analyis_server.cmd
@@ -1,3 +1,17 @@
 set SVN_REVISION=40638
 rmdir /s /q analysis_server
-svn co -r %SVN_REVISION% https://dart.googlecode.com/svn/branches/bleeding_edge/dart/pkg/analysis_server
+if errorlevel 1 goto error
+rmdir /s /q analyzer_clone
+if errorlevel 1 goto error
+call svn co -r %SVN_REVISION% https://dart.googlecode.com/svn/branches/bleeding_edge/dart/pkg/analysis_server
+call svn co -r %SVN_REVISION% https://dart.googlecode.com/svn/branches/bleeding_edge/dart/pkg/analyzer analyzer_clone
+
+goto end
+
+:error
+echo "Error. Try running again."
+goto end
+
+:end
+
+
